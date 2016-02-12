0220c8d20559da1a30d8d43924f8a68680605a6a
Heungsub Lee <sub@subl.ee>
2016-02-12 17:20:03 +0900

    Implement recursive watch for mock

--- etc/mock.py
+++ etc/mock.py
@@ -18,7 +18,7 @@ import six
 from six.moves import reduce
 
 from .adapters import Adapter
-from .errors import KeyNotFound
+from .errors import KeyNotFound, TimedOut
 from .results import Created, Deleted, Directory, Got, Set, Value
 
 
@@ -28,6 +28,24 @@ from .results import Created, Deleted, Directory, Got, Set, Value
 #     index2: node2
 #     ...
 # nodes:
+
+
+class Waiter(object):
+
+    def __init__(self):
+        self.event = threading.Event()
+
+    def set(self, value):
+        self.value = value
+        self.event.set()
+
+    def get(self, timeout=None):
+        if not self.event.wait(timeout):
+            raise TimedOut
+        return self.value
+
+    def is_set(self):
+        return self.event.is_set()
 
 
 class MockNode(object):
@@ -101,51 +119,67 @@ class MockNodeSnapshot(object):
 class MockAdapter(Adapter):
 
     def __init__(self, __):
-        self.events = {}
         self.index = 0
         self.root = MockNode('/', self.index, dir=True)
+        self.waiters = {}
 
     def next_index(self):
         self.index += 1
         return self.index
 
-    def wake_waiters(self, key):
+    def wake_waiters(self, key, result):
+        waiter_keys = [key]
+        key_chunks = self.split_key(key)
+        waiter_keys.extend(key_chunks[:x] for x in range(1, len(key_chunks)))
+        for waiter_key in waiter_keys:
+            try:
+                waiter = self.waiters.pop(waiter_key)
+            except KeyError:
+                pass
+            else:
+                waiter.set(result)
+
+    def make_result(self, result_class, node, *args, **kwargs):
         try:
-            event = self.events[key]
+            wake_key = kwargs.pop('wake')
         except KeyError:
-            pass
-        else:
-            event.set()
-            self.events.pop(key, None)
+            wake_key = None
+        c_node = node.canonicalize(*args, **kwargs)
+        result = result_class(c_node, None, self.index)
+        if wake_key:
+            self.wake_waiters(wake_key, result)
+        return result
 
     def split_key(self, key):
         if key == '/':
             return ()
         key_chunks = os.path.split(key)
-        if key_chunks[0] == '/':
-            key_chunks = list(key_chunks)
-            key_chunks[1] = '/' + key_chunks[1]
-            return key_chunks[1:]
-        else:
+        if key_chunks[0] != '/':
             return key_chunks
+        key_chunks = list(key_chunks)
+        key_chunks[1] = '/' + key_chunks[1]
+        return tuple(key_chunks[1:])
 
     def get(self, key, recursive=False, sorted=False, quorum=False,
             wait=False, wait_index=None, timeout=None):
         key_chunks = self.split_key(key)
         if wait:
-            while True:
+            try:
                 node = reduce(MockNode.get_node, key_chunks, self.root)
-                if wait_index <= node.modified_index:
-                    break
-                event = self.events.setdefault(key, threading.Event())
-                event.wait()
+            except KeyError:
+                pass
+            else:
+                wait = node.modified_index < wait_index
+            if wait:
+                waiter_key = key_chunks if recursive else key
+                waiter = self.waiters.setdefault(waiter_key, Waiter())
+                return waiter.get(timeout)
         else:
             try:
                 node = reduce(MockNode.get_node, key_chunks, self.root)
             except KeyError:
                 raise KeyNotFound(index=self.index)
-        c_node = node.canonicalize(wait_index, sorted=sorted)
-        return Got(c_node, None, self.index)
+        return self.make_result(Got, node, wait_index, sorted=sorted)
 
     def set(self, key, value=None, dir=False, ttl=None,
             prev_value=None, prev_index=None, prev_exist=None, timeout=None):
@@ -162,30 +196,25 @@ class MockAdapter(Adapter):
             parent_node.add_node(node)
         else:
             node.set(index, value, ttl, expiration)
-        self.wake_waiters(key)
-        c_node = node.canonicalize()
-        return Set(c_node, None, self.index)
+        return self.make_result(Set, node, wake=key)
 
     def append(self, key, value=None, dir=False, ttl=None, timeout=None):
         expiration = ttl and (datetime.utcnow() + timedelta(ttl))
         key_chunks = self.split_key(key)
-        parent_node = reduce(MockNode.get_node, key_chunks[:-1], self.root)
+        parent_node = reduce(MockNode.get_node, key_chunks, self.root)
         for x in itertools.count(len(parent_node.nodes)):
-            item_key = '%16d' % x
+            item_key = '%020d' % x
             if not parent_node.has_node(item_key):
                 break
+        key = os.path.join(key, item_key)
         index = self.next_index()
-        node = MockNode(os.path.join(key, item_key),
-                        index, value, dir, ttl, expiration)
+        node = MockNode(key, index, value, dir, ttl, expiration)
         parent_node.add_node(node)
-        self.wake_waiters(key)
-        c_node = node.canonicalize()
-        return Created(c_node, None, self.index)
+        return self.make_result(Created, node, wake=key)
 
     def delete(self, key, dir=False, recursive=False,
                prev_value=None, prev_index=None, timeout=None):
         key_chunks = self.split_key(key)
         parent_node = reduce(MockNode.get_node, key_chunks[:-1], self.root)
         node = parent_node.pop_node(key_chunks[-1])
-        c_node = node.canonicalize()
-        return Deleted(c_node, None, self.index)
+        return self.make_result(Deleted, node)
