8277d259c72608a189b26a3212333718604cc76d
Heungsub Lee <sub@subl.ee>
2016-11-03 20:51:04 +0900

    Bump version: 0.0.4

--- etc/__about__.py
+++ etc/__about__.py
@@ -3,9 +3,9 @@
    etc.__about__
    ~~~~~~~~~~~~~
 """
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __license__ = 'BSD'
 __author__ = 'Heungsub Lee'
 __author_email__ = 'sub@subl.ee'
 __url__ = 'https://github.com/sublee/etc'
-__description__ = 'An etcd client library for humans'
+__description__ = 'An etcd Python client library'
